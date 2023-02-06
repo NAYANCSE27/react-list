756cb27bb06e99a0105045c7ec4bab598a9c881d
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-06 22:27:44 +0600

    added element using props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Input from './input/Input';
 import './App.css';
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5];
   return (
     <div className="App">
+      <Input numbers = {numbers}/>
     </div>
   );
 }
