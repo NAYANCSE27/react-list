5399095a370f00865faf00d2719db8237ddfd127
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-10 20:37:06 +0600

    added file but not working

--- src/output/Output.jsx
+++ src/output/Output.jsx
@@ -3,23 +3,10 @@ import "./Output.css";
 function Output(){
     return( 
       <div className="table__value">
-      <table border={2} width="50%" cellPadding={10}>
-        <tbody>
-          <tr>
-            <td>Name</td>
-            <td>Fame</td>
-          </tr>
-          {inputarray.map((info, ind) => {
-            return (
-              <tr key={ind}>
-                <td>{info.name}</td>
-                <td>{info.fame}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+        list.map(function(item)
+          return (<li>{item}</li>);
+        )
+      </div>
     );
 }
 
